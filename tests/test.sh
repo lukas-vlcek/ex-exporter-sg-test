37815cd42f6309cf7e8a785da4e6d3abfb3b3bd5
Lukáš Vlček <lukas.vlcek@gmail.com>
2017-06-27 14:10:26 +0200

    Try access Prometheus REST endpoint

--- tests/test.sh
+++ tests/test.sh
@@ -3,10 +3,20 @@
 source ./tests/setup/env_init.sh
 
 echo "================================================="
-echo " Testing script"
+echo " List installed plugins"
 echo "================================================="
 
-curl -vs 'https://localhost:9200/_cat/nodes?v' \
+curl -vs 'https://localhost:9200/_cat/plugins?v' \
+  --cacert ${TMP_DIR}/search-guard-ssl/example-pki-scripts/ca/chain-ca.pem \
+  --cert ${TMP_DIR}/search-guard-ssl/example-pki-scripts/kirk.crt.pem \
+  --key  ${TMP_DIR}/search-guard-ssl/example-pki-scripts/kirk.key.pem
+
+
+echo "================================================="
+echo " Try Prometheus REST endpoint"
+echo "================================================="
+
+curl -vs 'https://localhost:9200/_prometheus/metrics' \
   --cacert ${TMP_DIR}/search-guard-ssl/example-pki-scripts/ca/chain-ca.pem \
   --cert ${TMP_DIR}/search-guard-ssl/example-pki-scripts/kirk.crt.pem \
   --key  ${TMP_DIR}/search-guard-ssl/example-pki-scripts/kirk.key.pem
