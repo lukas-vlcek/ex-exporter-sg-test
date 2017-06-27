044e376ce682f0da6153cc1487b6bbdf4cbcf257
Lukáš Vlček <lukas.vlcek@gmail.com>
2017-06-27 14:18:47 +0200

    Try if accessing Prometheus endpoint as non-admin fails

--- tests/test.sh
+++ tests/test.sh
@@ -13,10 +13,16 @@ curl -vs 'https://localhost:9200/_cat/plugins?v' \
 
 
 echo "================================================="
-echo " Try Prometheus REST endpoint"
+echo " Try Prometheus REST endpoint as kirk user"
 echo "================================================="
 
-curl -vs 'https://localhost:9200/_prometheus/metrics' \
+curl -vsS 'https://localhost:9200/_prometheus/metrics' \
   --cacert ${TMP_DIR}/search-guard-ssl/example-pki-scripts/ca/chain-ca.pem \
   --cert ${TMP_DIR}/search-guard-ssl/example-pki-scripts/kirk.crt.pem \
   --key  ${TMP_DIR}/search-guard-ssl/example-pki-scripts/kirk.key.pem
+
+echo "================================================="
+echo " Try Prometheus REST endpoint as worf user"
+echo "================================================="
+
+curl -vsS --insecure -u worf:worf 'https://localhost:9200/_prometheus/metrics'
