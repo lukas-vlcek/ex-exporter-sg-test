10e268a545119ebac32197ec9db1243d0f5803e7
Lukáš Vlček <lukas.vlcek@gmail.com>
2017-06-27 13:16:51 +0200

    First testing script

---
+++ tests/test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo "================================================="
+echo " Testing script"
+echo "================================================="
+
+curl -vs 'https://localhost:9200/_cat/nodes?v' \
+  --cacert ${TMP_DIR}/search-guard-ssl/example-pki-scripts/ca/chain-ca.pem \
+  --cert ${TMP_DIR}/search-guard-ssl/example-pki-scripts/kirk.crt.pem \
+  --key  ${TMP_DIR}/search-guard-ssl/example-pki-scripts/kirk.key.pem
