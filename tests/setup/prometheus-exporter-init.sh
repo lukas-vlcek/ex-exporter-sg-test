ad401cafc352a5586f82f7164d2d5487c64eb78c
Lukáš Vlček <lukas.vlcek@gmail.com>
2017-06-27 14:01:07 +0200

    Switch travis to Java

--- tests/setup/prometheus-exporter-init.sh
+++ tests/setup/prometheus-exporter-init.sh
@@ -8,5 +8,7 @@ echo "================================================="
 echo " Setup elasticsearch-prometheus-exporter"
 echo "================================================="
 
+java -version
+
 [[ -d ${ES_HOME}/bin/plugin/prometheus-exporter ]] && rm -rf ${ES_HOME}/bin/plugin/prometheus-exporter
 ${ES_HOME}/bin/plugin install https://github.com/vvanholl/elasticsearch-prometheus-exporter/releases/download/${PROMETHEUS_EXPORTER_VER}/elasticsearch-prometheus-exporter-${PROMETHEUS_EXPORTER_VER}.zip
